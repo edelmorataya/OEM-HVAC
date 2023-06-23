c352004521ae165142a2895246d73c603c510908
Edel Morataya <edelmorataya@gmail.com>
2023-06-23 16:03:12 -0600

    express

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ app.get("/", function (req, res) {
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
