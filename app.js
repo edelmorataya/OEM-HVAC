26e4631835e113e2f1044e2e4f849c7fa3a845fb
Edel Morataya <edelmorataya@gmail.com>
2023-06-23 16:06:51 -0600

    express

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ app.get("/", function (req, res) {
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
 
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  })
+const cors = require("cors");
+
+app.use(cors());
